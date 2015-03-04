5480967361520b3ac9973181c5df79d8477cdc2a
Vitche Research Team <developer@vitche.com>
2015-03-04 17:55:57 +0200

    1. Added a method capable to append non-existing methods.

--- patch.js
+++ patch.js
@@ -1,5 +1,12 @@
-module.exports.replace = function(context, methodName, method) {
-    if (undefined != context[methodName]) {
-        context[methodName] = method;
+module.exports = {
+    replace: function (context, methodName, method) {
+        if (undefined != context[methodName]) {
+            context[methodName] = method;
+        }
+    },
+    append: function (context, methodName, method) {
+        if (undefined == context[methodName]) {
+            context[methodName] = method;
+        }
     }
 };
